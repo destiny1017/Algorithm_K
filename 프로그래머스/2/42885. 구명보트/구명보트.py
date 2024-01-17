900c162694089f3cf0c681a9ebb9f9c3400fd6bc
KimDaeho <eogh6428@naver.com>
2024-01-17 23:00:44 +0900

    [level 2] Title: 구명보트, Time: 15.37 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -3,15 +3,12 @@ def solution(people, limit):
     answer = 0
     people.sort()
     min, max = 0, len(people) - 1
-    while min < max:
+    while min <= max:
         if people[min] + people[max] > limit:
             max -= 1
         else:
             min += 1
             max -= 1
-        answer += 1
-    
-    if min == max:
         answer += 1
     
     return answer
