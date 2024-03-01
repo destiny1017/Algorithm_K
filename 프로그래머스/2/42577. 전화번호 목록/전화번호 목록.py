867684bc707cb262f10fa76ac043bd7d71fe2083
KimDaeho <eogh6428@naver.com>
2024-03-01 21:52:24 +0900

    [level 2] Title: 전화번호 목록, Time: 469.95 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,12 +1,11 @@
 def solution(phone_book):
     phone_set = set(phone_book)
-    tmp_set = phone_set.copy()
     for item in phone_set:
-        tmp_set.remove(item)
+        phone_set.remove(item)
         for i in range(len(item)):
             split_str = item[0:len(item) - i]
-            if split_str in tmp_set:
+            if split_str in phone_set:
                 return False
-        tmp_set.add(item)
-    
+        phone_set.add(item)
+        
     return True
