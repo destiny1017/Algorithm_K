c110f5993644f7845c4620f3f0fbccb976e232d3
KimDaeho <eogh6428@naver.com>
2024-04-15 22:29:51 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,21 +1,11 @@
-# 알고리즘 : 최단거리, BFS 
-# 시간복잡도 : 고려X
-# 구현 과정
-# 1. begin 각 index를 순회하며 bfs 수행
-# 2. 현재 index의 문자를 변경할 수 있는 모든 단어를 queue에 추가
-# 3. 추가된 단어가 있으면 현재 index visit True, cnt + 1, 
-#    추가된 단어 없으면 index + 1 후 2번 수행
-# 4. queue 빼며 visit False인 문자 변경 가능한 경우 queue에 추가, 2~3반복
-
 from collections import deque
 
-def bfs(begin, target, words):
+def solution(begin, target, words):
     queue = deque()
     queue.append((begin, 0))
 
     while queue:
         change_word, cnt = queue.popleft()
-        
         if change_word == target:
             return cnt
         
@@ -27,10 +17,4 @@ def bfs(begin, target, words):
                     if "".join(tmp_words) == change_word:
                         queue.append((word, cnt+1))
                         words.remove(word)
-    
     return 0
-    
-
-def solution(begin, target, words):
-    answer = bfs(begin, target, words)
-    return answer
