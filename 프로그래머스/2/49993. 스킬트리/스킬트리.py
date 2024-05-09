2a00434d6d24e8690c725ba7a363d2d858b4f4ab
KimDaeho <eogh6428@naver.com>
2024-05-09 21:44:28 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -2,22 +2,13 @@ def solution(skill, skill_trees):
     answer = 0
     
     for tree in skill_trees:
-        prev_idx = 0
-        miss = False
-        able = True
-        for s in skill:
-            idx = tree.find(s)
-            if miss and idx >= 0:
-                able = False
-                break
-            if idx == -1:
-                miss = True
-            elif idx < prev_idx:
-                able = False
-                break
-            else:
-                prev_idx = idx
-        if able:
-            answer += 1
+        filter_tree = ""
+        for t in tree:
+            if t in skill:
+                filter_tree += t
+        
+        if filter_tree in skill:
+            if skill.find(filter_tree) == 0:
+                answer += 1
     
     return answer
