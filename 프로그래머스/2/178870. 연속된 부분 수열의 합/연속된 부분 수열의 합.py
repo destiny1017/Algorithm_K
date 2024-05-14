ba51d9e46fe6b5ff5f7168ca396a79084f57c6b2
KimDaeho <eogh6428@naver.com>
2024-05-14 22:42:13 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,12 +1,13 @@
 def solution(sequence, k):
-    answer = []
-    s = e = len(sequence) - 1
-    s_idx = 0
-    e_idx = e
+    # 배열 끝에서 시작
+    s = e = len(sequence) - 1 
+    s_idx, e_idx = 0, e
     n = sequence[e]
-    while s >= 0 <= e:
-        if n == k:
-            #print(f"n={n}, s={s}, e={e}")
+    
+    # 부분수열의 합이 k보다 작거나 같으면 s를, 크면 e를 줄이기 반복. 0이 될 때까지
+    while s > -1 < e:
+        if n == k: 
+            # 부분수열 합이 k와 같고 기존 배열보다 길이가 짧다면 인덱스 교체
             if abs(s - e) <= abs(s_idx - e_idx):
                 s_idx, e_idx = s, e
             s -= 1
