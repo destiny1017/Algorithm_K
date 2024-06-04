e5c400213ba5eb286a8eba1cb701e2b193f0769c
KimDaeho <eogh6428@naver.com>
2024-06-04 22:13:53 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -4,17 +4,14 @@ def solution(storey):
     value = 100000000
     
     while storey > 0:
-        val = abs(storey - value)
-        val2 = abs(storey - (value // 10))
-        if val > val2:
-            value //= 10
-            continue
-        if val >= storey:
+        val = abs(storey - value) # 현재 값에서 value를 뺀 절대값 구하기
+
+        # 절대값이 현재 값보다 더 커지거나, 한단계 아래 버튼으로 뺀 것보다 크면 버튼 차감
+        if val >= storey or val > abs(storey - (value // 10)):
             value //= 10
             continue
         
         storey = val
         answer += 1
-        # print(f"storey={storey}, val={val}, value={value}, cnt={answer}")
     
     return answer
