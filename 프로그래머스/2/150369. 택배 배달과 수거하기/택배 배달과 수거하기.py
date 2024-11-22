4f70a2bdf2d05da41c3ca45127533b1d3971702f
KimDaeho <eogh6428@naver.com>
2024-11-22 20:22:21 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 1628.30 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,22 +1,39 @@
-def working(work_stack, cap):
-    while work_stack:
-        pop = work_stack.pop()
-        if pop <= cap:
-            cap -= pop
-        else:
-            pop -= cap
-            work_stack.append(pop)
-            return work_stack
-    return work_stack
-
 def solution(cap, n, deliveries, pickups):
     answer = 0
-    working(deliveries, 0)
-    working(pickups, 0)
-    while deliveries or pickups:
+    d_idx = n - 1
+    p_idx = n - 1
+
+    # 뒤에서부터 처리할 위치를 찾아야 하므로 뒤에서부터 순회
+    while d_idx >= 0 or p_idx >= 0:
+        # 가장 먼 거리 계산
+        while d_idx >= 0 and deliveries[d_idx] == 0:
+            d_idx -= 1
+        while p_idx >= 0 and pickups[p_idx] == 0:
+            p_idx -= 1
+        
+        # 가장 먼 거리 기준 왕복 거리 추가
+        farthest = max(d_idx, p_idx) + 1
+        answer += farthest * 2
+
+        # 남은 용량으로 처리
         now_cap = cap
-        answer += max(len(deliveries), len(pickups)) * 2
-        deliveries = working(deliveries, now_cap)
-        pickups = working(pickups, now_cap)
+        while d_idx >= 0 and now_cap > 0:
+            if deliveries[d_idx] <= now_cap:
+                now_cap -= deliveries[d_idx]
+                deliveries[d_idx] = 0
+                d_idx -= 1
+            else:
+                deliveries[d_idx] -= now_cap
+                now_cap = 0
+        
+        now_cap = cap
+        while p_idx >= 0 and now_cap > 0:
+            if pickups[p_idx] <= now_cap:
+                now_cap -= pickups[p_idx]
+                pickups[p_idx] = 0
+                p_idx -= 1
+            else:
+                pickups[p_idx] -= now_cap
+                now_cap = 0
 
     return answer
