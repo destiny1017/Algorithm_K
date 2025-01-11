b0a527e9209eae2aa99b211cfad41c68ed408a49
KimDaeho <eogh6428@naver.com>
2025-01-12 03:58:13 +0900

    [level 2] Title: 숫자 변환하기, Time: 1300.80 ms, Memory: 203 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -7,21 +7,16 @@ def solution(x, y, n):
     
     while queue:
         idx, val = queue.popleft()
-        # print(idx, val)
         if val in sets:
             continue
-        else:
-            sets.add(val)
+        sets.add(val)
         
         if val == y:
             answer = min(answer, idx)
-        else:
-            if val * 3 <= y:
-                queue.append((idx+1, val*3))
-            if val * 2 <= y:
-                queue.append((idx+1, val*2))
-            if val + n <= y:
-                queue.append((idx+1, val+n))
+        elif val < y:
+            queue.append((idx+1, val*3))
+            queue.append((idx+1, val*2))
+            queue.append((idx+1, val+n))
     
     if answer == 10**6:
         return -1
