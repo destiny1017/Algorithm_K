220b96ff351bd1e5167c832c583b69341ed49793
KimDaeho <eogh6428@naver.com>
2025-02-27 22:33:11 +0900

    [level 2] Title: 호텔 대실, Time: 2.61 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,12 +1,20 @@
+import heapq
+
 def solution(book_time):
     answer = 0
-    use_times = [0] * 1451
+    heap = []
+    book_time.sort(key=lambda x: x[0])
     for b in book_time:
         st = b[0].split(":")
         ed = b[1].split(":")
         st_time = int(st[0]) * 60 + int(st[1])
         ed_time = int(ed[0]) * 60 + int(ed[1]) + 10
-        for i in range(st_time, ed_time):
-            use_times[i] += 1
-    # print(use_times[:60])
-    return max(use_times)
+        
+        if heap and heap[0] <= st_time:
+            heapq.heappop(heap)
+        else:
+            answer += 1
+        
+        heapq.heappush(heap, ed_time)
+
+    return answer
