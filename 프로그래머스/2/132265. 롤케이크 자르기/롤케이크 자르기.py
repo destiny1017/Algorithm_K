0c6689276c4941844db3a95cea4a994b1fb67df0
KimDaeho <eogh6428@naver.com>
2025-03-10 21:13:51 +0900

    [level 2] Title: 롤케이크 자르기, Time: 1020.70 ms, Memory: 51.3 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -7,21 +7,20 @@ def solution(topping):
             map1[t] = 0
         map1[t] += 1
     
-    map1_len = len(map1)
-    map2_len = 0
     for i in range(len(topping)):
         if topping[i] not in map2:
             map2[topping[i]] = 0
-            map2_len += 1
+
         map2[topping[i]] += 1
         map1[topping[i]] -= 1
-        if map1[topping[i]] == 0:
-            map1_len -= 1
         
-        if map1_len == map2_len:
+        if map1[topping[i]] == 0:
+            del map1[topping[i]]
+        
+        if len(map1) == len(map2):
             answer += 1
         
-        if map1_len < map2_len:
+        if len(map1) < len(map2):
             break
         
         
