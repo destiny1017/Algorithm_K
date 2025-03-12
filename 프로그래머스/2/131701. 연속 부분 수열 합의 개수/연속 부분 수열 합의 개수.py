38defde0aa85a36b80ccde6fe9d8ea4f97a86c19
KimDaeho <eogh6428@naver.com>
2025-03-12 20:59:14 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 302.97 ms, Memory: 42.6 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,12 +1,6 @@
+from itertools import accumulate
 
 def solution(elements):
-    ll = len(elements)
-    res = set()
-
-    for i in range(ll):
-        ssum = elements[i]
-        res.add(ssum)
-        for j in range(i+1, i+ll):
-            ssum += elements[j%ll]
-            res.add(ssum)
-    return len(res)
+    elements.extend(elements)
+    print(elements)
+    return 1
