039aea46c4a8f869b7f6a2173512885d22443359
KimDaeho <eogh6428@naver.com>
2025-03-19 19:49:19 +0900

    [Silver II] Title: 주식, Time: 2596 ms, Memory: 158980 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -1,17 +1,14 @@
 import sys
-from itertools import accumulate
 
 def calc_profit(n, arr):
-    accum = list(accumulate(arr))
-    dp = [0] * n
-    for i in range(1, n):
-        j = i
-        while j > 0 and arr[j-1] < arr[i]:
-            j -= 1
-        sum = accum[i-1] - accum[j-1] if j > 0 else accum[i-1]
-        dp[i] = dp[j-1] + (arr[i] * (i - j) - sum)
-
-    return dp[-1]
+    profit = 0
+    high = -1
+    for i in reversed(range(n)):
+        if arr[i] < arr[high]:
+            profit += arr[high] - arr[i]
+        elif arr[i] > arr[high]:
+            high = i
+    return profit
 
 input = sys.stdin.readline
 num = int(input())
